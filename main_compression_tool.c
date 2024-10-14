aefbe7a693a4cc89c3a58f9760e3708e424456ca
tun70323 <tun70323@temple.edu>
2024-10-14 17:05:57 -0400

    Implement pipes and work processes creation

--- main_compression_tool.c
+++ main_compression_tool.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+// Function to decompress files
 void decompress_files(const char *filename) {
     int retval = fork();
     if (retval < 0) {
@@ -20,5 +21,28 @@ void decompress_files(const char *filename) {
 }
 
 int main() {
+    decompress_files("testfiles.tar.gz");
+
+    int pipes[4][2][2]; // [worker][input/output][read/write]
+
+    // Create pipes for 4 workers
+    for(int i = 0; i < 4; i++) {
+        if (pipe(pipes[i][0]) == -1 || pipe(pipes[i][1]) == -1) {
+            perror("pipe failed");
+            exit(1);
+        }
+    }
+
+    // Fork worker processes
+    for(int i = 0; i < 4; i++) {
+        int retval = fork();
+        if (retval < 0) {
+            perror("fork failed");
+            exit(1);
+        } else if (retval == 0) {
+            exit(0);
+        }
+    }
+
     return 0;
 }
