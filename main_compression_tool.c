cdf76df2076f3fb18253e74f55712263f859848f
tun70323 <tun70323@temple.edu>
2024-10-22 19:36:08 -0400

    Implement file distribution function

--- main_compression_tool.c
+++ main_compression_tool.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <dirent.h>
 
 // Function to decompress files
 void decompress_files(const char *filename) {
@@ -21,7 +22,36 @@ void decompress_files(const char *filename) {
     }
 }
 
+// Function to distribute files to workers
+void distribute_files(const char *directory, int pipes[4][2][2]) {
+    DIR *dir = opendir(directory);
+    if (dir == NULL) {
+        perror("opendir failed");
+        exit(1);
+    }
+
+    struct dirent *entry;
+    int worker_id = 0;
+
+    // Search directory items
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_name[0] == '.') // Skip files that start with .
+            continue;
+        
+        char filepath[512];
+        snprintf(filepath, sizeof(filepath), "%s/%s", directory, entry->d_name);
+
+        printf("Sending file %s to worker %d\n", filepath, worker_id + 1);
+        dprintf(pipes[worker_id][0][1], "%s\n", filepath);
+
+        worker_id = (worker_id + 1) % 4;
+    }
+
+    closedir(dir);
+}
+
 int main() {
+    printf("Starting file decompression...\n");
     decompress_files("testfiles.tar.gz");
 
     int pipes[4][2][2]; // [worker][input/output][read/write]
@@ -42,19 +72,34 @@ int main() {
             perror("fork failed");
             exit(1);
         } else if (retval == 0) {
+            close(pipes[i][0][1]);
+            close(pipes[i][1][0]);
+
+            dup2(pipes[i][0][0], STDIN_FILENO);
+            close(pipes[i][0][0]);
+
+            dup2(pipes[i][1][1], STDOUT_FILENO);
+            close(pipes[i][1][1]);
+
             printf("Worker %d (PID: %d) created.\n", i + 1, getpid());
+
+            while (1) {
+                char filepath[256];
+                if (scanf("%s", filepath) !=EOF) {
+                    printf("Received file path: %s\n", filepath);
+                }
+            }
             exit(0);
         } else {
+            close(pipes[i][0][0]);
+            close(pipes[i][1][1]);
+
             printf("Worker %d (PID: %d) forked by main process.\n", i + 1, retval);
         }
     }
 
-    for (int i = 0; i < 4; i++) {
-        int status;
-        wait(&status);
-        printf("Worker %d finished.\n", i + 1);
-    }
+    distribute_files("./decompressed_files", pipes);
 
-    printf("All workers finished, exiting main process.\n");
+    printf("All files sent, main process exiting.\n");
     return 0;
 }
